e17bcdb4384f8f19b6288a1527a3ec4389bdb6f0
Daniel Knies <inf1911@hs-worms.de>
2017-06-17 15:48:58 +0200

    Fix: Statusleiste nun korrekt eingefärbt
    Signed-off-by: Daniel Knies <inf1911@hs-worms.de>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -54,7 +54,8 @@ export class MyApp {
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
-      this.statusBar.styleDefault();
+      this.statusBar.overlaysWebView(true);
+      this.statusBar.backgroundColorByHexString("#1976D2");
       this.splashScreen.hide();
     });
   }
